7ebb768b1114107b6be7893ea6a93689fcce8715
wasimraazakram <89869508+wasimraazakram@users.noreply.github.com>
2023-10-10 22:47:13 +0530

    Time: 115 ms (27.87%), Space: 72 MB (13.05%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second>=2)
+            return true;
+        }
+        return false;
+        
+    }
+};
