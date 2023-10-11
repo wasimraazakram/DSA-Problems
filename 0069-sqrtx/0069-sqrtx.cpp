0c27c9c2894e1e888f6e8709f08796f14b17b5c5
wasimraazakram <89869508+wasimraazakram@users.noreply.github.com>
2023-10-11 14:01:16 +0530

    Time: 0 ms (100%), Space: 6.5 MB (6.27%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long low=1,high=x;
+        long long mid;
+
+        while(low<=high){
+            mid=(low+high)/2;
+            long long val=mid*mid;
+            if(val<=x){
+                low=mid+1;
+            }
+            else
+            high=mid-1;
+        }
+        return high;
+        
+    }
+};
