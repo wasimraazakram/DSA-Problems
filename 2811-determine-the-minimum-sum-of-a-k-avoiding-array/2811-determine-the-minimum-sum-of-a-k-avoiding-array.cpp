c120ad2967bc6543c361b79b5bfe468c0cfecd86
wasimraazakram <89869508+wasimraazakram@users.noreply.github.com>
2023-11-01 23:25:38 +0530

    Time: 0 ms (100%), Space: 6.2 MB (83.32%) - LeetHub

---
+++ 2811-determine-the-minimum-sum-of-a-k-avoiding-array/2811-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+   int calculateSumInRange(int start, int count) {
+        int sum = (count * (2 * start + (count - 1))) / 2;
+        return sum;
+    }
+    
+    int minimumSum(int quantity, int k) {
+        int middle = k / 2;
+        
+        if (quantity <= middle) {
+            return quantity * (quantity + 1) / 2;
+        }
+        
+        int leftSum = middle * (middle + 1) / 2;
+        
+        int remainingNumbers = quantity - middle;
+        
+        return leftSum + calculateSumInRange(k, remainingNumbers);
+    }
+};
